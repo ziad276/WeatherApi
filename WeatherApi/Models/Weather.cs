bafc08df3184870b374b52bc79ece33fad93df1b
ziad gouhar <ziadgouhar53@gmail.com>
2026-02-07 18:46:20 +0200

    Initial commit: minimal Weather API setup

---
+++ WeatherApi/Models/Weather.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherApi.Models
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public int Temperature { get; set; }
+        public string Season { get; set; }
+    }
+}
---
+++ WeatherApi/Models/WeatherDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WeatherApi.Models
+{
+    public class WeatherDb: DbContext
+    {
+        public WeatherDb(DbContextOptions<WeatherDb> options)
+        : base(options) { }
+
+        public DbSet<Weather> Weathers => Set<Weather>();
+
+    }
+}
